ece803a5a99e2b8d7fa2a75192264e263f825040
ran@win10 <raniero.beber@strath.ac.uk>
2022-08-08 14:40:04 +0200

    Added "Hello World!" Homework

---
+++ homeworks/hello_world/task_1/hello.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//----------------------------|
+// Author   >>>   RBeber      |
+// Date:    >>>   08/08/2022  |
+// last modified: 08/08/2022  |
+//----------------------------|
+int main() {
+  puts("Hello World!");
+  return 0;
+}
