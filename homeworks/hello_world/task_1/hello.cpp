1dfad3dba7c90ef813d05c120c02acb6059f0a5d
ran@win10 <raniero.beber@strath.ac.uk>
2022-08-08 14:57:57 +0200

    removed personal heading and RECOMMIT

--- homeworks/hello_world/task_1/hello.cpp
+++ homeworks/hello_world/task_1/hello.cpp
@@ -1,9 +1,5 @@
 #include <stdio.h>
-//----------------------------|
-// Author   >>>   RBeber      |
-// Date:    >>>   08/08/2022  |
-// last modified: 08/08/2022  |
-//----------------------------|
+
 int main() {
   puts("Hello World!");
   return 0;
